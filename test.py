9177a0936e80415ae6990981f3f07cb48830dd3d
Yang Zhaonan <yangzhaonan18@qq.com>
2019-08-10 17:34:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -27,29 +27,31 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     dataset = ListDataset(path, img_size=img_size, augment=False, multiscale=False)
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
-    )
+    )   # 验证和测试数据集时 shuffle=False， 因为设置成True 没有意义
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
-    labels = []
-    sample_metrics = []  # List of tuples (TP, confs, pred)
+    labels = []  # 用来存放 所有图片 的检测结果的标签
+    sample_metrics = []  # List of tuples (TP, confs, pred)  # 记录所有图片的检测结果
     for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
+        #  targets 是多张图片 的检测结果
+        #  targets [:, 0] 和1 值得是什么
 
         # Extract labels
-        labels += targets[:, 1].tolist()
+        labels += targets[:, 1].tolist()  # 将所有对象的类别编号存放在列表labels中 ?????????????
         # Rescale target
-        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
-        targets[:, 2:] *= img_size
+        targets[:, 2:] = xywh2xyxy(targets[:, 2:])  # 坐标转换，将YOLO中心点表示的比例坐标 转换成 两个点表示的比例坐标
+        targets[:, 2:] *= img_size  # 比例坐标乘以图片的尺寸 等于 点的坐标值（计算出来的坐标值应该是小数，按理说应该是整数）
 
         imgs = Variable(imgs.type(Tensor), requires_grad=False)
 
         with torch.no_grad():
-            outputs = model(imgs)
-            outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
+            outputs = model(imgs)  # 输入 多张图片 预测结果
+            outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)  # 对一张图片的检测结果 进行非极大抑制
 
-        sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
+        sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)  # 记录所有图片的 检测结果
 
-    # Concatenate sample statistics
+    # Concatenate sample statistics  连接 样本 统计信息
     true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
     precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
 
@@ -73,15 +75,15 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    data_config = parse_data_config(opt.data_config)
-    valid_path = data_config["valid"]
-    class_names = load_classes(data_config["names"])
+    data_config = parse_data_config(opt.data_config)  # 解析配置文件 coco.data 中的信息，返回的是字典
+    valid_path = data_config["valid"]  # 验证数据集TXT  valid=data/coco/5k.txt “data/coco/5k.txt”
+    class_names = load_classes(data_config["names"])  # 读取配置文件中，指定的coco name的名称列表
 
     # Initiate model
-    model = Darknet(opt.model_def).to(device)
+    model = Darknet(opt.model_def).to(device)  # 加载YOLOv3(Darknet)模型
     if opt.weights_path.endswith(".weights"):
         # Load darknet weights
-        model.load_darknet_weights(opt.weights_path)
+        model.load_darknet_weights(opt.weights_path)  # 加载模型参数  weights/yolov3.weights
     else:
         # Load checkpoint weights
         model.load_state_dict(torch.load(opt.weights_path))
